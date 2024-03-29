bcb63821539b8209babc03fa80ea15dbe501abab
kathy393 <kathyyang393@gmail.com>
2024-03-29 07:08:38 -0400

    Run Black formatter

--- plot_charts.py
+++ plot_charts.py
@@ -7,6 +7,7 @@ import pandas as pd
 import seaborn as sns
 from adjustText import adjust_text
 
+
 def average_color_bubble():
     """
     Generates colored bubble chart, with x- and y- axis as the first two
@@ -34,29 +35,35 @@ def average_color_bubble():
     plt.xlim(0, 1)
     plt.ylim(0, 1)
 
-    #create legend for 'Instrumentalness' color values
+    # create legend for 'Instrumentalness' color values
     plt.colorbar(averages_scatter, label="Instrumentalness")
 
-    #create legend for 'Speechiness' size values
+    # create legend for 'Speechiness' size values
     plt.scatter([], [], s=0, label="Speechiness")
     size_labels = [0.2, 0.15, 0.1, 0.05]
     for size in size_labels:
         plt.scatter([], [], s=size * 3000, label=f"{size}", alpha=0.5)
     plt.legend()
 
-    #label each point with artist name
+    # label each point with artist name
     texts = []
-    for i, artist_name in enumerate(average_df['artist_name']):
-        texts.append(plt.text(average_df['valence'][i],
-                              average_df['acousticness'][i], artist_name))
+    for i, artist_name in enumerate(average_df["artist_name"]):
+        texts.append(
+            plt.text(
+                average_df["valence"][i],
+                average_df["acousticness"][i],
+                artist_name,
+            )
+        )
     adjust_text(texts)
 
     plt.show()
 
+
 def artist_sums_bar():
     """
-    Generates bar charts, measuring artist's total value for each feature. 
-    The x-axis is artists, and y-axis is the sum of their songs' values for 
+    Generates bar charts, measuring artist's total value for each feature.
+    The x-axis is artists, and y-axis is the sum of their songs' values for
     the feature of the chart.
 
     Args:
@@ -68,13 +75,12 @@ def artist_sums_bar():
     all_data = pd.read_csv("all_tracks.csv")
     all_df = pd.DataFrame(all_data)
 
-    grouped = all_df.groupby('artist_name').sum()
+    grouped = all_df.groupby("artist_name").sum()
 
-    #finds sum for each feature, new values placed in own dataframe
-    summed_df = grouped[['valence',
-                         'acousticness', 
-                         'instrumentalness', 
-                         'speechiness']]
+    # finds sum for each feature, new values placed in own dataframe
+    summed_df = grouped[
+        ["valence", "acousticness", "instrumentalness", "speechiness"]
+    ]
 
     _, axes = plt.subplots(1, 4, figsize=(15, 5))
 
@@ -82,11 +88,12 @@ def artist_sums_bar():
         ax = axes[i]
         ax.bar(summed_df.index, summed_df[feature])
         ax.set_title(feature)
-        ax.set_xlabel('Artist')
-        ax.set_ylabel('Sum of ' + feature)
-        ax.tick_params(axis='x', rotation=90)
+        ax.set_xlabel("Artist")
+        ax.set_ylabel("Sum of " + feature)
+        ax.tick_params(axis="x", rotation=90)
     plt.tight_layout()
     plt.show()
+
 
 def correlation_matrix():
     """
@@ -101,23 +108,26 @@ def correlation_matrix():
     all_data = pd.read_csv("all_tracks.csv")
     all_df = pd.DataFrame(all_data)
 
-    columns_to_include = ['valence', 
-                          'acousticness',
-                          'instrumentalness', 
-                          'speechiness']
+    columns_to_include = [
+        "valence",
+        "acousticness",
+        "instrumentalness",
+        "speechiness",
+    ]
     numeric_df = all_df[columns_to_include]
 
     corr_matrix = numeric_df.corr()
 
     plt.figure(figsize=(8, 6))
-    plt.imshow(corr_matrix, cmap='coolwarm', interpolation='nearest')
-    plt.colorbar(label='Correlation coefficient')
-    plt.title('Correlation Matrix')
+    plt.imshow(corr_matrix, cmap="coolwarm", interpolation="nearest")
+    plt.colorbar(label="Correlation coefficient")
+    plt.title("Correlation Matrix")
     plt.xticks(np.arange(len(corr_matrix)), corr_matrix.columns, rotation=45)
     plt.yticks(np.arange(len(corr_matrix)), corr_matrix.columns)
-    plt.xlabel('Features')
-    plt.ylabel('Features')
+    plt.xlabel("Features")
+    plt.ylabel("Features")
     plt.show()
+
 
 def track_scatterplot_matrix():
     """
@@ -132,14 +142,18 @@ def track_scatterplot_matrix():
     all_data = pd.read_csv("all_tracks.csv")
     all_df = pd.DataFrame(all_data)
 
-    matrix = pd.plotting.scatter_matrix(all_df[['valence', 'acousticness',
-                                       'instrumentalness', 'speechiness']], 
-                                       figsize=(10, 10), marker='o', alpha=0.7)
+    matrix = pd.plotting.scatter_matrix(
+        all_df[["valence", "acousticness", "instrumentalness", "speechiness"]],
+        figsize=(10, 10),
+        marker="o",
+        alpha=0.7,
+    )
     for ax in matrix.ravel():
         ax.set_xlim(0, 1)
         ax.set_ylim(0, 1)
 
     plt.show()
+
 
 def feature_box_plot():
     """
@@ -154,10 +168,12 @@ def feature_box_plot():
     all_data = pd.read_csv("all_tracks.csv")
     all_df = pd.DataFrame(all_data)
 
-    columns_to_keep = ['valence',
-                       'acousticness', 
-                       'instrumentalness', 
-                       'speechiness']    
+    columns_to_keep = [
+        "valence",
+        "acousticness",
+        "instrumentalness",
+        "speechiness",
+    ]
     subset_df = all_df[columns_to_keep]
     feature_columns = subset_df
 
@@ -169,52 +185,62 @@ def feature_box_plot():
         plt.title(feature + " of all songs")
 
     plt.tight_layout()
-    plt.show() 
+    plt.show()
+
 
 def single_artist_box():
     """
     Generates box plots for all artists for each feature.
-    
+
     Args:
         None.
-    
+
     Returns:
         None.
     """
     all_data = pd.read_csv("all_tracks.csv")
     all_df = pd.DataFrame(all_data)
 
-    df = all_df[['artist_name',
-                 'valence', 
-                 'acousticness', 
-                 'instrumentalness', 
-                 'speechiness']]
-    unique_artists = df['artist_name'].unique()
+    df = all_df[
+        [
+            "artist_name",
+            "valence",
+            "acousticness",
+            "instrumentalness",
+            "speechiness",
+        ]
+    ]
+    unique_artists = df["artist_name"].unique()
 
     _, axes = plt.subplots(4, 9, figsize=(15, 9), sharex=True)
 
-    min_value = df[['valence', 'acousticness', 'instrumentalness', 'speechiness']].min().min()
-    max_value = df[['valence', 'acousticness', 'instrumentalness', 'speechiness']].max().max()
+    min_value = (
+        df[["valence", "acousticness", "instrumentalness", "speechiness"]]
+        .min()
+        .min()
+    )
+    max_value = (
+        df[["valence", "acousticness", "instrumentalness", "speechiness"]]
+        .max()
+        .max()
+    )
 
-    #iterate for each artist
+    # iterate for each artist
     for i, artist in enumerate(unique_artists):
-        artist_df = df[df['artist_name'] == artist]
+        artist_df = df[df["artist_name"] == artist]
         if pd.isnull(artist):
             continue
-        #iterate over each feature
-        for j, feature in enumerate(['valence',
-                                     'acousticness', 
-                                     'instrumentalness', 
-                                     'speechiness']):
-            sns.boxplot(y=artist_df[feature], ax=axes[j,i])
+        # iterate over each feature
+        for j, feature in enumerate(
+            ["valence", "acousticness", "instrumentalness", "speechiness"]
+        ):
+            sns.boxplot(y=artist_df[feature], ax=axes[j, i])
             axes[j, i].set_ylim(min_value, max_value)
 
             if i == 0:
-                axes[j,i].set_ylabel(feature)
+                axes[j, i].set_ylabel(feature)
             if j == 0:
-                axes[j,i].set_title(artist)
+                axes[j, i].set_title(artist)
 
     plt.tight_layout()
     plt.show()
-
-single_artist_box()
--- song_artist_data.py
+++ song_artist_data.py
@@ -10,9 +10,10 @@ from spotipy.oauth2 import SpotifyClientCredentials
 import api_keys
 
 client_credentials_manager = SpotifyClientCredentials(
-                                client_id=api_keys.CLIENT_ID,
-                                client_secret=api_keys.CLIENT_SECRET)
+    client_id=api_keys.CLIENT_ID, client_secret=api_keys.CLIENT_SECRET
+)
 sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
+
 
 def get_artist_top_songs(artist, sp):
     """
@@ -24,41 +25,42 @@ def get_artist_top_songs(artist, sp):
         artist_name: a string representing the name of the artist to search.
         sp: a function from Spotipy that manages client credential information
             necessary for accessing the API.
-    
+
     Returns:
-        A list with general information of the top ten songs of the ten 
+        A list with general information of the top ten songs of the ten
         most popular artists.
     """
     tracks_info = []
     all_tracks_info = []
 
-    #search for artist
-    results = sp.search(q='artist:' + artist, type='artist')
-    if len(results['artists']['items']) == 0:
+    # search for artist
+    results = sp.search(q="artist:" + artist, type="artist")
+    if len(results["artists"]["items"]) == 0:
         print(f"No artist found with the name '{artist}'.")
         return
 
-    #get artist ID
-    artist_id = results['artists']['items'][0]['id']
+    # get artist ID
+    artist_id = results["artists"]["items"][0]["id"]
 
-    #get top tracks
+    # get top tracks
     time.sleep(10)
     top_tracks = sp.artist_top_tracks(artist_id)
 
-    #extract information
-    for track in top_tracks['tracks']:
+    # extract information
+    for track in top_tracks["tracks"]:
         track_info = {
-            'artist_name': artist,
-            'name': track['name'],
-            'track_id': track['id'],
-            'popularity': track['popularity'],
-            'album_name': track['album']['name'],
-            'album_release_date': track['album']['release_date'],
+            "artist_name": artist,
+            "name": track["name"],
+            "track_id": track["id"],
+            "popularity": track["popularity"],
+            "album_name": track["album"]["name"],
+            "album_release_date": track["album"]["release_date"],
         }
         tracks_info.append(track_info)
     all_tracks_info.extend(tracks_info)
 
     return all_tracks_info
+
 
 def get_track_features(track_id, sp):
     """
@@ -71,35 +73,36 @@ def get_track_features(track_id, sp):
         sp: an authenticated instance of the Spotipy client.
 
     Returns:
-        A dictionary containing audio features of the track obtained from the 
-        Spotify API. If the track features are unavailable or if an error 
+        A dictionary containing audio features of the track obtained from the
+        Spotify API. If the track features are unavailable or if an error
         occurs, returns None.
     """
-    time.sleep(10) # sleep to prevent hitting spotify api rate limits
+    time.sleep(10)  # sleep to prevent hitting spotify api rate limits
     features = sp.audio_features(track_id)
 
     return features[0]
 
+
 def find_average(data_list):
     """
-    Finds the average or mode of a list. 
-    
-    If the list contains all number (int or float), the function finds mean, 
+    Finds the average or mode of a list.
+
+    If the list contains all number (int or float), the function finds mean,
     and if values are strings, the function uses mode
-    
+
     Args:
         list : a list containing numeric or string values.
 
     Returns:
-        An average if the list's elements are numeric, and a string of the 
+        An average if the list's elements are numeric, and a string of the
         mode if the list's elements are strings.
 
     """
     if len(data_list) > 0:
-        if isinstance(list[0], (float, int)): # using mean
+        if isinstance(list[0], (float, int)):  # using mean
             return sum(list) / len(list)
-        if isinstance(list[0], str): # using mode
-            counts = {value:0 for value in data_list}
+        if isinstance(list[0], str):  # using mode
+            counts = {value: 0 for value in data_list}
 
             for value in data_list:
                 counts[value] += 1
@@ -110,36 +113,38 @@ def find_average(data_list):
                 if value == max_value:
                     return key
 
+
 features_to_include = [
     "song_name",
     "artist_name",
     "acousticness",
     "instrumentalness",
     "speechiness",
-    "valence"
+    "valence",
 ]
+
 
 def get_artist_average_features(ten_songs, sp):
     """
     Retrieve and compute the average audio features for an artist's top songs.
 
-    Iterates through the list of top songs for an artist, retrieves their 
-    audio features using the Spotify API, and calculates the average of each 
+    Iterates through the list of top songs for an artist, retrieves their
+    audio features using the Spotify API, and calculates the average of each
     feature across all songs for each artist.
 
     This function utilizes time.sleep() to avoid hitting API rate limits.
-    
+
     The function depends on the get_track_features and find_average functions.
-    
+
     Args:
-        top_songs: a list of dictionaries containing information about the 
+        top_songs: a list of dictionaries containing information about the
             artist's top songs, including a "track_id".
         sp: an authenticated instance of the Spotipy client.
 
     Returns:
-        A dictionary containing the average audio features for the artist's 
-        top songs. The keys are the audio features ("acousticness", 
-        "instrumentalness", "speechiness", and "valence"), and the values 
+        A dictionary containing the average audio features for the artist's
+        top songs. The keys are the audio features ("acousticness",
+        "instrumentalness", "speechiness", and "valence"), and the values
         represent the artist's respective average values of their top songs.
     """
     artist_song_features = {}
@@ -159,7 +164,7 @@ def get_artist_average_features(ten_songs, sp):
             existing_values.append(value)
 
     average_features = {
-        k:find_average(v) for k, v in artist_song_features.items()
+        k: find_average(v) for k, v in artist_song_features.items()
     }
     return average_features
 
@@ -168,22 +173,22 @@ def write_data_to_csv(artists_features, file_name, keys=features_to_include):
     """
     Write artist features to a CSV file.
 
-    This function takes a dictionary of artist features generated from 
+    This function takes a dictionary of artist features generated from
     get_artist_average_features(). It writes this data to a CSV file.
 
-    The function excludes certain keys ("type", "id", "uri", "track_href", 
-    "analysis_url") from the data that are not applicable for analysis before 
+    The function excludes certain keys ("type", "id", "uri", "track_href",
+    "analysis_url") from the data that are not applicable for analysis before
     writing to the CSV file.
-    
-    Each row in the CSV file represents an artist, and columns corresponds 
+
+    Each row in the CSV file represents an artist, and columns corresponds
     to their feature values.
 
     Args:
-        artists_features: a dictionary where the keys are artist names and the 
-            values are dictionaries containing feature information. The nested 
-            dictionary contains the feature name as a key, and the data as a 
+        artists_features: a dictionary where the keys are artist names and the
+            values are dictionaries containing feature information. The nested
+            dictionary contains the feature name as a key, and the data as a
             value.
-            
+
         file_name: the name of the CSV file to write the data to.
 
     Returns:
@@ -192,26 +197,34 @@ def write_data_to_csv(artists_features, file_name, keys=features_to_include):
     exclude = ["type", "id", "uri", "track_href", "analysis_url"]
 
     data = {
-        artist_name: {
-            k:v for k, v in features.items() if k not in exclude
-        } for artist_name, features in artists_features.items()
+        artist_name: {k: v for k, v in features.items() if k not in exclude}
+        for artist_name, features in artists_features.items()
     }
 
-    data = [{
-        "artist_name": artist_name,
-        **features
-    } for artist_name, features in data.items()]
+    data = [
+        {"artist_name": artist_name, **features}
+        for artist_name, features in data.items()
+    ]
 
-
-    with open(file_name, 'w+', newline='', encoding="utf-8") as file:
+    with open(file_name, "w+", newline="", encoding="utf-8") as file:
         dict_writer = csv.DictWriter(file, keys + ["artist_name"])
         dict_writer.writeheader()
         dict_writer.writerows(data)
 
+
 if __name__ == "__main__":
-    artist_names = ['The Weeknd', 'Taylor Swift', 'Ariana Grande', 'Rihanna',
-                    'Drake', 'Kanye West', 'Justin Bieber', 'Dua Lipa', 
-                    'Coldplay', 'Bruno Mars']
+    artist_names = [
+        "The Weeknd",
+        "Taylor Swift",
+        "Ariana Grande",
+        "Rihanna",
+        "Drake",
+        "Kanye West",
+        "Justin Bieber",
+        "Dua Lipa",
+        "Coldplay",
+        "Bruno Mars",
+    ]
 
     artists_features = {}
     artists_songs = []
@@ -220,8 +233,8 @@ if __name__ == "__main__":
         top_songs = get_artist_top_songs(artist_name, sp)
         artists_songs.extend(top_songs)
         artists_features[artist_name] = get_artist_average_features(
-                                                                top_songs, sp)
+            top_songs, sp
+        )
 
     write_data_to_csv(artists_features, "artists_average.csv")
     write_data_to_csv(artists_songs, "artists_songs.csv")
-    
