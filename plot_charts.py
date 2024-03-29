9ff309dcd32ce5447a80a71c3dbd1a0b47ab615c
kathy393 <kathyyang393@gmail.com>
2024-03-29 05:13:11 -0400

    Fix previous charts and create new charts
    Fixed unconsistent axis for graphs and other errors.
    Created new chart for box plot by feature for all artists

--- plot_charts.py
+++ plot_charts.py
@@ -4,6 +4,7 @@ module for creating graphs
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
+import seaborn as sns
 from adjustText import adjust_text
 
 def average_color_bubble():
@@ -30,6 +31,9 @@ def average_color_bubble():
     plt.ylabel("Acousticness")
     plt.title("Average of Song Features for All Artists")
 
+    plt.xlim(0, 1)
+    plt.ylim(0, 1)
+
     #create legend for 'Instrumentalness' color values
     plt.colorbar(averages_scatter, label="Instrumentalness")
 
@@ -48,6 +52,7 @@ def average_color_bubble():
     adjust_text(texts)
 
     plt.show()
+
 def artist_sums_bar():
     """
     Generates bar charts, measuring artist's total value for each feature. 
@@ -66,10 +71,12 @@ def artist_sums_bar():
     grouped = all_df.groupby('artist_name').sum()
 
     #finds sum for each feature, new values placed in own dataframe
-    summed_df = grouped[['valence', 'acousticness', 'instrumentalness', 'speechiness']]
+    summed_df = grouped[['valence',
+                         'acousticness', 
+                         'instrumentalness', 
+                         'speechiness']]
 
-    #plot
-    fig, axes = plt.subplots(1, 4, figsize=(40, 8))  # 1 row, 4 columns for 4 features
+    _, axes = plt.subplots(1, 4, figsize=(15, 5))
 
     for i, feature in enumerate(summed_df.columns):
         ax = axes[i]
@@ -77,13 +84,27 @@ def artist_sums_bar():
         ax.set_title(feature)
         ax.set_xlabel('Artist')
         ax.set_ylabel('Sum of ' + feature)
+        ax.tick_params(axis='x', rotation=90)
     plt.tight_layout()
     plt.show()
+
 def correlation_matrix():
+    """
+    Generates correlation_matrix for all of the features.
+
+    Args:
+        None.
+
+    Returns:
+        None.
+    """
     all_data = pd.read_csv("all_tracks.csv")
     all_df = pd.DataFrame(all_data)
 
-    columns_to_include = ['valence', 'acousticness', 'instrumentalness', 'speechiness']
+    columns_to_include = ['valence', 
+                          'acousticness',
+                          'instrumentalness', 
+                          'speechiness']
     numeric_df = all_df[columns_to_include]
 
     corr_matrix = numeric_df.corr()
@@ -97,60 +118,103 @@ def correlation_matrix():
     plt.xlabel('Features')
     plt.ylabel('Features')
     plt.show()
-    return
-def tracks_color_bubble():
+
+def track_scatterplot_matrix():
     """
-    Generates colored bubble chart, with x- and y- axis as the first two
-    variables, color as the third variable, and the size of bubble as fourth
-    point.
+    Create a scatteplot matrix for all songs.
 
     Args:
         None.
 
     Returns
+        None.
     """
     all_data = pd.read_csv("all_tracks.csv")
     all_df = pd.DataFrame(all_data)
 
-    color_map = plt.cm.get_cmap('tab10')
-
-    # Extract unique artist names
-    artists = all_df['artist_name'].unique()
-
-    # Assign a unique color to each artist
-    colors = {artist: color_map(i) for i, artist in enumerate(artists)}
-
-    # Scatterplot matrix
-    pd.plotting.scatter_matrix(all_df[['valence', 'acousticness', 'instrumentalness', 'speechiness']], figsize=(10, 10), c=all_df['artist_name'].apply(lambda x: colors[x]), marker='o', alpha=0.7)
-
-    # Add legend
-    legend_entries = [plt.Line2D([0], [0], marker='o', color='w', label=artist, markersize=8, markerfacecolor=colors[artist]) for artist in artists]
-    plt.legend(handles=legend_entries, loc='upper left')
+    matrix = pd.plotting.scatter_matrix(all_df[['valence', 'acousticness',
+                                       'instrumentalness', 'speechiness']], 
+                                       figsize=(10, 10), marker='o', alpha=0.7)
+    for ax in matrix.ravel():
+        ax.set_xlim(0, 1)
+        ax.set_ylim(0, 1)
 
     plt.show()
+
 def feature_box_plot():
+    """
+    Generates box plots using all tracks for each feature.
+
+    Args:
+        None.
+
+    Returns:
+        None.
+    """
     all_data = pd.read_csv("all_tracks.csv")
     all_df = pd.DataFrame(all_data)
 
-    columns_to_keep = ['valence', 'acousticness', 'instrumentalness', 'speechiness']    
+    columns_to_keep = ['valence',
+                       'acousticness', 
+                       'instrumentalness', 
+                       'speechiness']    
     subset_df = all_df[columns_to_keep]
+    feature_columns = subset_df
 
-    # Extracting only the columns relevant for features
-    feature_columns = subset_df  # Assuming features start from the 3rd column
-
-    # Plotting boxplots for each feature
-    plt.figure(figsize=(5, 10))  # Adjust the figure size if necessary
+    plt.figure(figsize=(10, 5))
 
     for i, feature in enumerate(feature_columns, 1):
-        plt.subplot(2, 2, i)  # Adjust subplot layout according to the number of features
+        plt.subplot(1, 4, i)
         plt.boxplot(all_df[feature])
         plt.title(feature + " of all songs")
 
-    plt.tight_layout()  # Adjust subplot layout to prevent overlapping
+    plt.tight_layout()
+    plt.show() 
+
+def single_artist_box():
+    """
+    Generates box plots for all artists for each feature.
+    
+    Args:
+        None.
+    
+    Returns:
+        None.
+    """
+    all_data = pd.read_csv("all_tracks.csv")
+    all_df = pd.DataFrame(all_data)
+
+    df = all_df[['artist_name',
+                 'valence', 
+                 'acousticness', 
+                 'instrumentalness', 
+                 'speechiness']]
+    unique_artists = df['artist_name'].unique()
+
+    _, axes = plt.subplots(4, 9, figsize=(15, 9), sharex=True)
+
+    min_value = df[['valence', 'acousticness', 'instrumentalness', 'speechiness']].min().min()
+    max_value = df[['valence', 'acousticness', 'instrumentalness', 'speechiness']].max().max()
+
+    #iterate for each artist
+    for i, artist in enumerate(unique_artists):
+        artist_df = df[df['artist_name'] == artist]
+        if pd.isnull(artist):
+            continue
+        #iterate over each feature
+        for j, feature in enumerate(['valence',
+                                     'acousticness', 
+                                     'instrumentalness', 
+                                     'speechiness']):
+            sns.boxplot(y=artist_df[feature], ax=axes[j,i])
+            axes[j, i].set_ylim(min_value, max_value)
+
+            if i == 0:
+                axes[j,i].set_ylabel(feature)
+            if j == 0:
+                axes[j,i].set_title(artist)
+
+    plt.tight_layout()
     plt.show()
 
-average_color_bubble()
-artist_sums_bar()
-correlation_matrix()
-tracks_color_bubble()
-feature_box_plot()
+single_artist_box()
